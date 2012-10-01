2f254239a65931b5d2c0db829c959085a6d340f0
Chris Sarbora <chris.sarbora@docusign.com>
2012-10-01 12:06:29 -0700

    Adding isEditText

--- src/com/tophyr/csftests/CSFActivityTestCase.java
+++ src/com/tophyr/csftests/CSFActivityTestCase.java
@@ -425,6 +425,10 @@ public class CSFActivityTestCase<StartingActivity extends Activity> extends Acti
 		return isType(in, TextView.class);
 	}
 	
+	protected FindViewResult<EditText> isEditText(FindViewResult<? extends View> in) {
+		return isType(in, EditText.class);
+	}
+	
 	protected <T extends View, R extends View> FindViewResult<R> isType(FindViewResult<T> result, final Class<R> type) {
 		result.description = String.format("%s that are %ss", result.description, type.getSimpleName());
 		
